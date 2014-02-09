125686d5c5f8366ffc13dda0e383dca323f6cc50
Peter Souter <p.morsou@gmail.com>
2014-02-09 02:11:34 +0000

    More basics taken from chef install...

--- install_puppet.sh
+++ install_puppet.sh
@@ -16,6 +16,14 @@ exists() {
   fi
 }
 
+# Helper bug-reporting text
+report_bug() {
+  echo "Please file a bug report at https://github.com/petems/puppet-install-shell/"
+  echo "Version: $version"
+  echo " "
+  echo "Please detail your operating system type, version and any other relevant details"
+}
+
 # Get command line arguments
 while getopts pv:f:d:P: opt
 do
@@ -36,3 +44,348 @@ shift `expr $OPTIND - 1`
 
 machine=`uname -m`
 os=`uname -s`
+
+# Retrieve Platform and Platform Version
+if test -f "/etc/lsb-release" && grep -q DISTRIB_ID /etc/lsb-release; then
+  platform=`grep DISTRIB_ID /etc/lsb-release | cut -d "=" -f 2 | tr '[A-Z]' '[a-z]'`
+  platform_version=`grep DISTRIB_RELEASE /etc/lsb-release | cut -d "=" -f 2`
+elif test -f "/etc/debian_version"; then
+  platform="debian"
+  platform_version=`cat /etc/debian_version`
+elif test -f "/etc/redhat-release"; then
+  platform=`sed 's/^\(.\+\) release.*/\1/' /etc/redhat-release | tr '[A-Z]' '[a-z]'`
+  platform_version=`sed 's/^.\+ release \([.0-9]\+\).*/\1/' /etc/redhat-release`
+
+  # If /etc/redhat-release exists, we act like RHEL by default
+  if test "$platform" = "fedora"; then
+    # Change platform version for use below.
+    platform_version="6.0"
+  fi
+  platform="el"
+elif test -f "/etc/system-release"; then
+  platform=`sed 's/^\(.\+\) release.\+/\1/' /etc/system-release | tr '[A-Z]' '[a-z]'`
+  platform_version=`sed 's/^.\+ release \([.0-9]\+\).*/\1/' /etc/system-release | tr '[A-Z]' '[a-z]'`
+  # amazon is built off of fedora, so act like RHEL
+  if test "$platform" = "amazon linux ami"; then
+    platform="el"
+    platform_version="6.0"
+  fi
+# Apple OS X
+elif test -f "/usr/bin/sw_vers"; then
+  platform="mac_os_x"
+  # Matching the tab-space with sed is error-prone
+  platform_version=`sw_vers | awk '/^ProductVersion:/ { print $2 }'`
+
+  major_version=`echo $platform_version | cut -d. -f1,2`
+  case $major_version in
+    "10.6") platform_version="10.6" ;;
+    "10.7"|"10.8"|"10.9") platform_version="10.7" ;;
+    *) echo "No builds for platform: $major_version"
+       report_bug
+       exit 1
+       ;;
+  esac
+
+  # x86_64 Apple hardware often runs 32-bit kernels (see OHAI-63)
+  x86_64=`sysctl -n hw.optional.x86_64`
+  if test $x86_64 -eq 1; then
+    machine="x86_64"
+  fi
+elif test -f "/etc/release"; then
+  platform="solaris2"
+  machine=`/usr/bin/uname -p`
+  platform_version=`/usr/bin/uname -r`
+elif test -f "/etc/SuSE-release"; then
+  if grep -q 'Enterprise' /etc/SuSE-release;
+  then
+      platform="sles"
+      platform_version=`awk '/^VERSION/ {V = $3}; /^PATCHLEVEL/ {P = $3}; END {print V "." P}' /etc/SuSE-release`
+  else
+      platform="suse"
+      platform_version=`awk '/^VERSION =/ { print $3 }' /etc/SuSE-release`
+  fi
+elif test "x$os" = "xFreeBSD"; then
+  platform="freebsd"
+  platform_version=`uname -r | sed 's/-.*//'`
+elif test "x$os" = "xAIX"; then
+  platform="aix"
+  platform_version=`uname -v`
+  machine="ppc"
+fi
+
+if test "x$platform" = "x"; then
+  echo "Unable to determine platform version!"
+  report_bug
+  exit 1
+fi
+
+# Mangle $platform_version to pull the correct build
+# for various platforms
+major_version=`echo $platform_version | cut -d. -f1`
+case $platform in
+  "el")
+    platform_version=$major_version
+    ;;
+  "debian")
+    case $major_version in
+      "5") platform_version="6";;
+      "6") platform_version="6";;
+      "7") platform_version="6";;
+    esac
+    ;;
+  "freebsd")
+    platform_version=$major_version
+    ;;
+  "sles")
+    platform_version=$major_version
+    ;;
+  "suse")
+    platform_version=$major_version
+    ;;
+esac
+
+if test "x$platform_version" = "x"; then
+  echo "Unable to determine platform version!"
+  report_bug
+  exit 1
+fi
+
+if test "x$platform" = "xsolaris2"; then
+  # hack up the path on Solaris to find wget
+  PATH=/usr/sfw/bin:$PATH
+  export PATH
+fi
+
+checksum_mismatch() {
+  echo "Package checksum mismatch!"
+  report_bug
+  exit 1
+}
+
+unable_to_retrieve_package() {
+  echo "Unable to retrieve a valid package!"
+  report_bug
+  echo "Metadata URL: $metadata_url"
+  if test "x$download_url" != "x"; then
+    echo "Download URL: $download_url"
+  fi
+  if test "x$stderr_results" != "x"; then
+    echo "\nDEBUG OUTPUT FOLLOWS:\n$stderr_results"
+  fi
+  exit 1
+}
+
+capture_tmp_stderr() {
+  # spool up /tmp/stderr from all the commands we called
+  if test -f "/tmp/stderr"; then
+    output=`cat /tmp/stderr`
+    stderr_results="${stderr_results}\nSTDERR from $1:\n\n$output\n"
+    rm /tmp/stderr
+  fi
+}
+
+# do_wget URL FILENAME
+do_wget() {
+  echo "trying wget..."
+  wget -O "$2" "$1" 2>/tmp/stderr
+  rc=$?
+  # check for 404
+  grep "ERROR 404" /tmp/stderr 2>&1 >/dev/null
+  if test $? -eq 0; then
+    echo "ERROR 404"
+    unable_to_retrieve_package
+  fi
+
+  # check for bad return status or empty output
+  if test $rc -ne 0 || test ! -s "$2"; then
+    capture_tmp_stderr "wget"
+    return 1
+  fi
+
+  return 0
+}
+
+# do_curl URL FILENAME
+do_curl() {
+  echo "trying curl..."
+  curl -sL -D /tmp/stderr "$1" > "$2"
+  rc=$?
+  # check for 404
+  grep "404 Not Found" /tmp/stderr 2>&1 >/dev/null
+  if test $? -eq 0; then
+    echo "ERROR 404"
+    unable_to_retrieve_package
+  fi
+
+  # check for bad return status or empty output
+  if test $rc -ne 0 || test ! -s "$2"; then
+    capture_tmp_stderr "curl"
+    return 1
+  fi
+
+  return 0
+}
+
+# do_fetch URL FILENAME
+do_fetch() {
+  echo "trying fetch..."
+  fetch -o "$2" "$1" 2>/tmp/stderr
+  # check for bad return status
+  test $? -ne 0 && return 1
+  return 0
+}
+
+# do_curl URL FILENAME
+do_perl() {
+  echo "trying perl..."
+  perl -e 'use LWP::Simple; getprint($ARGV[0]);' "$1" > "$2" 2>/tmp/stderr
+  rc=$?
+  # check for 404
+  grep "404 Not Found" /tmp/stderr 2>&1 >/dev/null
+  if test $? -eq 0; then
+    echo "ERROR 404"
+    unable_to_retrieve_package
+  fi
+
+  # check for bad return status or empty output
+  if test $rc -ne 0 || test ! -s "$2"; then
+    capture_tmp_stderr "perl"
+    return 1
+  fi
+
+  return 0
+}
+
+# do_curl URL FILENAME
+do_python() {
+  echo "trying python..."
+  python -c "import sys,urllib2 ; sys.stdout.write(urllib2.urlopen(sys.argv[1]).read())" "$1" > "$2" 2>/tmp/stderr
+  rc=$?
+  # check for 404
+  grep "HTTP Error 404" /tmp/stderr 2>&1 >/dev/null
+  if test $? -eq 0; then
+    echo "ERROR 404"
+    unable_to_retrieve_package
+  fi
+
+  # check for bad return status or empty output
+  if test $rc -ne 0 || test ! -s "$2"; then
+    capture_tmp_stderr "python"
+    return 1
+  fi
+  return 0
+}
+
+do_checksum() {
+  if exists sha256sum; then
+    checksum=`sha256sum $1 | awk '{ print $1 }'`
+    if test "x$checksum" != "x$2"; then
+      checksum_mismatch
+    else
+      echo "Checksum compare with sha256sum succeeded."
+    fi
+  elif exists shasum; then
+    checksum=`shasum -a 256 $1 | awk '{ print $1 }'`
+    if test "x$checksum" != "x$2"; then
+      checksum_mismatch
+    else
+      echo "Checksum compare with shasum succeeded."
+    fi
+  elif exists md5sum; then
+    checksum=`md5sum $1 | awk '{ print $1 }'`
+    if test "x$checksum" != "x$3"; then
+      checksum_mismatch
+    else
+      echo "Checksum compare with md5sum succeeded."
+    fi
+  elif exists md5; then
+    checksum=`md5 $1 | awk '{ print $4 }'`
+    if test "x$checksum" != "x$3"; then
+      checksum_mismatch
+    else
+      echo "Checksum compare with md5 succeeded."
+    fi
+  else
+    echo "WARNING: could not find a valid checksum program, pre-install shasum, md5sum or md5 in your O/S image to get valdation..."
+  fi
+}
+
+# do_download URL FILENAME
+do_download() {
+  echo "downloading $1"
+  echo "  to file $2"
+
+  # we try all of these until we get success.
+  # perl, in particular may be present but LWP::Simple may not be installed
+
+  if exists wget; then
+    do_wget $1 $2 && return 0
+  fi
+
+  if exists curl; then
+    do_curl $1 $2 && return 0
+  fi
+
+  if exists fetch; then
+    do_fetch $1 $2 && return 0
+  fi
+
+  if exists perl; then
+    do_perl $1 $2 && return 0
+  fi
+
+  if exists python; then
+    do_python $1 $2 && return 0
+  fi
+
+  unable_to_retrieve_package
+}
+
+# install_file TYPE FILENAME
+# TYPE is "rpm", "deb", "solaris", or "sh"
+install_file() {
+  echo "Installing Puppet $version"
+  case "$1" in
+    "rpm")
+      echo "installing with rpm..."
+      rpm -Uvh --oldpackage --replacepkgs "$2"
+      ;;
+    "deb")
+      echo "installing with dpkg..."
+      dpkg -i "$2"
+      ;;
+    "solaris")
+      echo "installing with pkgadd..."
+      echo "conflict=nocheck" > /tmp/nocheck
+      echo "action=nocheck" >> /tmp/nocheck
+      echo "mail=" >> /tmp/nocheck
+      pkgrm -a /tmp/nocheck -n puppet >/dev/null 2>&1 || true
+      pkgadd -n -d "$2" -a /tmp/nocheck puppet
+      ;;
+    "sh" )
+      echo "installing with sh..."
+      sh "$2"
+      ;;
+    *)
+      echo "Unknown filetype: $1"
+      report_bug
+      exit 1
+      ;;
+  esac
+  if test $? -ne 0; then
+    echo "Installation failed"
+    report_bug
+    exit 1
+  fi
+}
+
+echo "Downloading Puppet $version for ${platform}..."
+
+if test "x$TMPDIR" = "x"; then
+  tmp="/tmp"
+else
+  tmp=$TMPDIR
+fi
+# secure-ish temp dir creation without having mktemp available (DDoS-able but not expliotable)
+tmp_dir="$tmp/install.sh.$$"
+(umask 077 && mkdir $tmp_dir) || exit 1
