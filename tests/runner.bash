322c67e40e05ecf84d1de1df9e948ba7ec561d07
Peter Souter <p.morsou@gmail.com>
2014-02-09 16:48:17 +0000

    Extremely simple bats tests added I'm pretty new to bats, so I'm not sure how to do more complex matching and helper setup. This'll do for now...

---
+++ tests/runner.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/env bats
+
+if [ ! -x "../install_puppet.sh" ]; then
+    printf "No executable ../install_puppet.sh found.\n" >&2
+    printf "It's either been renamed or something terrible has happened...\n" >&2
+    exit 1
+fi
+
+pi="../install_puppet.sh"
+
+@test "sanity" {
+    run true
+    [ "$status" -eq 0 ]
+    [ "$output" = "" ]
+}
+
+@test "helper option" {
+    run "$pi" -h
+    [ "$status" -eq 0 ]
+    [ "${lines[0]}" = "install_puppet.sh - A shell script to install Puppet, asumming no dependancies" ]
+}
+
+@test "illegal option" {
+    run "$pi" -x
+    [ "$status" -eq 1 ]
+    [ "${lines[0]}" = "../install_puppet.sh: illegal option -- x" ]
+}
