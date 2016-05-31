bf0b6c474e24510deeed959a295e5308df6f5c07
Peter Souter <petems@users.noreply.github.com>
2016-05-31 10:59:50 +0100

    Updating tiny version of Puppet 4.4

--- install_puppet_agent.sh
+++ install_puppet_agent.sh
@@ -195,7 +195,7 @@ else
       puppet_agent_version='1.3.5'
       ;;
     4.4.*)
-      puppet_agent_version='1.4.0'
+      puppet_agent_version='1.4.2'
       ;;
     *)
       critical "Unable to match requested puppet version to puppet-agent version - Check http://docs.puppetlabs.com/puppet/latest/reference/about_agent.html"
