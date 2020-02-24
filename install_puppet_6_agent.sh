0ba1447b8cc371c278abb64cf7fd0710e4c9ab48
Bram Vogelaar <attachmentgenie@inuits.eu>
2020-02-24 23:24:26 +0100

    puppet now also provides upstream debian 10 packages

--- install_puppet_6_agent.sh
+++ install_puppet_6_agent.sh
@@ -503,6 +503,7 @@ case $platform in
           "7") deb_codename="wheezy";;
           "8") deb_codename="jessie";;
           "9") deb_codename="stretch";;
+          "10") deb_codename="buster";;
         esac
         filetype="deb"
         filename="puppet6-release-${deb_codename}.deb"
